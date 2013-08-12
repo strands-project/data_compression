89a96bb3bc572020b5cea73b659fb8dad08e9980
Nils Bore <nbore@kth.se>
2013-08-12 18:09:57 +0200

    Added decompression of rgb images

--- libav_compressor/scripts/batch_decompressor.py
+++ libav_compressor/scripts/batch_decompressor.py
@@ -6,21 +6,25 @@ def batch_decompressor(impath):
     vidpath = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "videos"))
     counter = 0
     flist = os.listdir(vidpath)
+    flist = filter(lambda s: s[:5] == "depth", flist)
     flist.sort(key = lambda s: (s[:-10], int(s[-10:-4])))
     for f in flist:
-        vidfile = os.path.join(vidpath, f)
-        if not os.path.isfile(vidfile):
+        depthvid = os.path.join(vidpath, f)
+        if not os.path.isfile(depthvid):
             continue
-        if f[:5] != "depth":
-            continue
+        rgbvid = os.path.join(vidpath, "rgb" + f[-10:-4] + ".mov") # should this be mkv?
         depthimages = os.path.join(impath, "tempdepth%06d.tiff")
+        rgbimages = os.path.join(impath, "temprgb%06d.png")
         avconv = os.path.abspath(os.path.join(os.path.expanduser('~'), "libav", "bin", "avconv"))
-        os.system("%s -i %s -f image2 -pix_fmt gray16 %s" % (avconv, vidfile, depthimages))
+        os.system("%s -i %s -f image2 -pix_fmt gray16 %s" % (avconv, depthvid, depthimages))
+        os.system("%s -i %s -f image2 %s" % (avconv, rgbvid, rgbimages))
         templist = os.listdir(impath)
-        templist = filter(lambda s: s[:4] == "temp", templist)
+        templist = filter(lambda s: s[:4] == "tempdepth", templist)
         templist.sort(key = lambda s: int(s[-11:-5]))
         for t in templist:
+            rgbt = os.path.join(impath, "temprgb" + t[-11:5] + ".png")
             os.rename(os.path.join(impath, t), os.path.join(impath, "depth%06d" % counter))
+            os.rename(rgbt, os.path.join(impath, "rgb%06d" % counter))
             counter += 1
 
 if __name__ == "__main__":
