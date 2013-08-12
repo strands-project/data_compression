8753066ae36c2d58a0bf96266d7c586f3186b85e
Nils Bore <nbore@kth.se>
2013-08-12 14:26:00 +0200

    Added basic color support and association

--- libav_compressor/scripts/batch_compressor.py
+++ libav_compressor/scripts/batch_compressor.py
@@ -8,21 +8,30 @@ def callback(sc, impath, nbr):
     temps = []
     counter = 0
     flist = os.listdir(impath)
-    flist.sort(key = lambda s: (s[:-10], int(s[-10:-4])))
+    flist = filter(lambda s: s[:5] == "depth", flist)
+    flist.sort(key = lambda s: int(s[-10:-4]))
     for f in flist:
-        if not os.path.isfile(os.path.join(impath, f)):
+        if not os.path.isfile(os.path.join(impath, f)): # should not happen
             continue
-        if f[:5] != "depth":
-            continue
-        tempname = os.path.join(impath, "tempdepth%06d.png" % counter)
-        os.rename(os.path.join(impath, f), tempname)
-        #shutil.copy(tempname, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "debug", f))) # for debugging, saving the original images
-        temps.append(tempname)
+        #if f[:5] != "depth":
+            #continue
+        depthtemp = os.path.join(impath, "tempdepth%06d.png" % counter)
+        rgbtemp = os.path.join(impath, "temprgb%06d.png" % counter)
+        rgbf = "rgb" + f[-10:-4] + ".png"
+        os.rename(os.path.join(impath, f), depthtemp) # check if there really are depths coming in
+        os.rename(os.path.join(impath, rgbf), rgbtemp) # check if there really are rgbs coming in
+        #shutil.copy(depthtemp, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "debug", f))) # for debugging, saving the original images
+        #shutil.copy(rgbtemp, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "debug", rgbf))) # for debugging, saving the original images
+        temps.append(depthtemp)
+        temps.append(rgbtemp)
         counter += 1
     depthimages = os.path.join(impath, "tempdepth%06d.png")
+    rgbimages = os.path.join(impath, "temprgb%06d.png")
     avconv = os.path.abspath(os.path.join(os.path.expanduser('~'), "libav", "bin", "avconv"))
-    video = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "videos", "depth%06d.mov" % nbr))
-    os.system("%s -r 30 -i %s -pix_fmt gray16 -vsync 1 -vcodec ffv1 -coder 1 %s" % (avconv, depthimages, video))
+    depthvideo = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "videos", "depth%06d.mov" % nbr))
+    rgbvideo = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "videos", "rgb%06d.mov" % nbr))
+    os.system("%s -r 30 -i %s -pix_fmt gray16 -vsync 1 -vcodec ffv1 -coder 1 %s" % (avconv, depthimages, depthvideo))
+    os.system("%s -r 30 -i %s -c:v libx264 -preset ultrafast -crf 0 %s" % (avconv, rgbimages, rgbvideo))
     for f in temps:
         os.remove(f)
 
