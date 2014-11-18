d429e279017bbc9e7925ed689c2a4e0fdd84effb
Nils Bore <nbore@kth.se>
2014-11-18 21:47:09 +0100

    Covered all the cases with the action server and changed the names of the recorded topics to include the camera name

--- mongodb_openni_compression/scripts/record_server.py
+++ mongodb_openni_compression/scripts/record_server.py
@@ -6,6 +6,7 @@ import rospy
 #import threading
 
 import actionlib
+from actionlib_msgs.msg import GoalStatus
 from mongodb_openni_compression.msg import RecordCameraAction, RecordCameraResult, RecordCameraFeedback
 from roslaunch_axserver.msg import launchAction, launchGoal, launchResult, launchFeedback
 
@@ -23,11 +24,28 @@ class RoslaunchServer(object):
         rospy.loginfo("/record_camera action server started...")
 
     def cancel_cb(self, gh):
+        rospy.loginfo('cancel camera record goal ' + gh.get_goal_id().id)
         self.roslaunch_axclient.cancel_all_goals()
         gh.set_canceled()
-
+        self.gh = None
+        
+    def feedback_cb(self, feedback):
+        myfeedback = RecordCameraFeedback()
+        myfeedback.ready = feedback.ready
+        self.gh.publish_feedback(feedback)
+        
+    def done_cb(self, goal_status, result):
+        if self.gh is None:
+            return
+        if goal_status == GoalStatus.SUCCEEDED:
+            self.gh.set_succeeded()
+        else:
+            self.gh.set_aborted()
+        self.gh = None
+        
     def execute_cb(self, gh):
         gh.set_accepted()
+        self.gh = gh
         mygoal = gh.get_goal()
         goal = launchGoal()
         goal.pkg = "mongodb_openni_compression"
@@ -36,14 +54,9 @@ class RoslaunchServer(object):
             goal.launch_file = goal.launch_file + " with_depth:=false"
         if not mygoal.with_rgb:
             goal.launch_file = goal.launch_file + " with_rgb:=false"
+        #goal.monitored_topics = ["/" + mygoal.camera + "head_xtion/rgb/image_raw", "/" + mygoal.camera + "head_xtion/depth/image_raw"]
         print goal.pkg + " " + goal.launch_file
-        self.roslaunch_axclient.send_goal(goal)
-        #while (gh.get_goal_status().status == actionlib.GoalStatus.ACTIVE
-               # and not rospy.is_shutdown()):
-            #feedback = RecordCameraFeedback()
-            #feedback.ready = self.roslaunch_axclient.get_state() == actionlib.GoalStatus.ACTIVE
-            #gh.publish_feedback(feedback)
-            #rospy.sleep(1.0)
+        self.roslaunch_axclient.send_goal(goal, done_cb = self.done_cb, active_cb = None, feedback_cb = self.feedback_cb)
 
 if __name__ == '__main__':
     rospy.init_node('record_camera')
